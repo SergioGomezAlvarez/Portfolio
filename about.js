c698efc433c9c1a9bbddf4736f9c2edbe6593a3d
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-10-15 12:25:19 +0200

    Added a basic nav structure

--- about.js
+++ about.js
@@ -59,22 +59,22 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 3500);
 });
 
-// script.js
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const observerOptions = {
-    threshold: 0.1
+    threshold: 0.1,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting){
-        entry.target.classList.add('show');
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
         observer.unobserve(entry.target);
       }
     });
   }, observerOptions);
 
-  const hiddenElements = document.querySelectorAll('.card, .exp-container, .ad-app-container, .contact-container');
-  hiddenElements.forEach(el => observer.observe(el));
+  const hiddenElements = document.querySelectorAll(
+    ".card, .exp-container, .ad-app-container, .contact-container"
+  );
+  hiddenElements.forEach((el) => observer.observe(el));
 });
